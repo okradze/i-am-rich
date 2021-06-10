b6ee245e356a568177d9573694baf34f67343168
Mirian Okradze <okradzemirian@gmail.com>
2021-06-10 12:11:30 +0400

    initial

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Mirian on 09.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
